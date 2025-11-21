8c952d367e8fab72258cdfe1010eacfa3b318460
prdhk2 <pradhika2112@gmail.com>
2025-11-21 08:38:18 +0700

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment('production')) {
+         if (env('APP_ENV') != 'local') {
             URL::forceScheme('https');
         }
     }
